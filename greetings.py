35c4f3ebea05dc36b7df283a36d82401b43381f0
nikitimas <nikitimas342@gmai.com>
2024-12-22 20:18:16 +0300

    Grade 1

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Никита"
+title = "Заметка 1"
+content = "Список задач"
+status = "Не выполнено"
+created_date = "10-11-2024"
+issue_date = "10-12-2024"
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
